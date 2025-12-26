b28b43098d271ee3e5f6c47a97dba8ae7c061c2f
rvelez-dev <rachel.v062704@gmail.com>
2025-12-26 13:56:41 -0500

    Initial Commit

---
+++ lib/getLocation/modify_location.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class ModifyLocation extends StatefulWidget {
+  const ModifyLocation({super.key});
+
+  @override
+  State<ModifyLocation> createState() => _ModifyLocationState();
+}
+
+class _ModifyLocationState extends State<ModifyLocation> {
+
+  String _latitude = 'Latitude:';
+  String _longitude = 'Longitude:';
+
+  Future<void> fetchCurrentPosition() async
+  {
+    try
+    {
+      Position currentposition = await Geolocator.getCurrentPosition(
+        locationSettings: const LocationSettings(
+          accuracy: LocationAccuracy.high,
+        ),
+      );
+      setState(() {
+        _latitude = 'Latitude: ${currentposition.latitude}';
+        _longitude = 'Latitude: ${currentposition.longitude}';
+      });
+    }
+    catch (error)
+    {
+      print('Error: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation App"),
+        flexibleSpace: Container( //changes the top header
+          color: Colors.redAccent
+        ),
+        centerTitle: true, // centers title of app
+      ),
+      body:Container( // changes the body of the app
+        decoration: BoxDecoration(
+          color: Colors.black26,
+              //begin: Alignment.topLeft,
+              //end: Alignment.bottomRight,
+          ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: (){
+                    fetchCurrentPosition();
+                  },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.redAccent,
+                ),
+                child: const Text('Get Location'),
+              ),
+              const SizedBox(height:20,),
+              Text(
+                _latitude,
+                style:TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height:20,),
+              Text(
+                _longitude,
+                style:TextStyle(fontSize: 20),
+              ),
+            ],
+          )
+        )
+        ),
+      );
+  }
+}
