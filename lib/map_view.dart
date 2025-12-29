e891c1c46e80a4ca4e0edb495f07bbbd81276425
rvelez-dev <rachel.v062704@gmail.com>
2025-12-29 13:37:18 -0500

    Adding more packages

--- lib/map_view.dart
+++ lib/map_view.dart
@@ -2,6 +2,11 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:http/http.dart';
+import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
 
 class MapView extends StatelessWidget {
   const MapView({Key? key}) : super(key: key);
@@ -9,7 +14,11 @@ class MapView extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
   return Scaffold(
-    appBar: AppBar(title: Text('Map View')),
+    appBar: AppBar(
+        foregroundColor: Colors.white,
+        title: const Text('ESU Navigation'),
+        backgroundColor: Colors.redAccent,
+    ),
     body: FlutterMap(
       options: MapOptions(
         initialCenter: LatLng(40.9975, -75.1727), // Center the map
@@ -18,7 +27,7 @@ Widget build(BuildContext context) {
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.navigation_app', // Replace with your package
+          userAgentPackageName: 'com.example.navigation_app',
         ),
         RichAttributionWidget(
           attributions: [
