dd32d1a0ca90d3bdbefcdea2c8c1d52cd24741cc
rvelez-dev <rachel.v062704@gmail.com>
2025-12-30 12:28:21 -0500

    Creating a dropdown menu for user

---
+++ lib/campus_dropdown.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+// StatefulWidget class for dropdown
+class BuildingDropdown extends StatefulWidget {
+
+  final Function(String)? onSelected;
+
+  const BuildingDropdown({Key? key, this.onSelected}) : super(key: key);
+
+  @override
+  State<BuildingDropdown> createState() => _BuildingDropdownState();
+}
+
+//
+class _BuildingDropdownState extends State<BuildingDropdown> {
+  // Current selection
+  String? _selectedBuilding;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      hint: const Text("Select a building"),
+      value: _selectedBuilding,
+      items: [
+        // Placeholder options
+        DropdownMenuItem(value: "Library", child: Text("Library")),
+        DropdownMenuItem(value: "Science Hall", child: Text("Science Hall")),
+      ],
+      onChanged: (value) {
+        setState(() {
+          _selectedBuilding = value;
+        });
+
+        // Call the callback in MapView if provided
+        if (value != null && widget.onSelected != null) {
+          widget.onSelected!(value);
+        }
+      },
+    );
+  }
+}
